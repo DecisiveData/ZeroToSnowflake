fc64689bb01136b92df03905f3a3651940cb6122
Michael Grimm <michael@decisivedata.net>
2018-12-07 10:48:17 -0800

    new City, State, Country

--- salesforce/fivetran/dimensional/dim_account.sql
+++ salesforce/fivetran/dimensional/dim_account.sql
@@ -1,13 +1,16 @@
 CREATE OR REPLACE VIEW "DIM_ACCOUNT" COPY GRANTS AS
 SELECT
     --Key
-    ACC.ID          AS "Account Id"
+    ACC.ID                  AS "Account Id"
     --Attributes
-    ,ACC.NAME       AS "Account"
-    ,ACC.TYPE       AS "Type"
-    ,ACC.PHONE      AS "Phone"
-    ,ACC.WEBSITE    AS "Website"
-    ,ACC.INDUSTRY   AS "Industry"
+    ,ACC.NAME               AS "Account"
+    ,ACC.TYPE               AS "Type"
+    ,ACC.PHONE              AS "Phone"
+    ,ACC.WEBSITE            AS "Website"
+    ,ACC.INDUSTRY           AS "Industry"
+    ,ACC.BILLING_CITY       AS "City"
+    ,ACC.BILLING_STATE      AS "State"
+    ,ACC.BILLING_COUNTRY    AS "Country"
 FROM
     ACCOUNT ACC
 WHERE
