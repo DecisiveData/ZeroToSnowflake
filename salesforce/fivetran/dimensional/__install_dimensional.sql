0df221bd6d6c8c9919229d8db5db9a085b4420f4
Michael Grimm <michael@decisivedata.net>
2018-12-11 08:18:46 -0800

    updating dim_account in install_dimensional.sql

--- salesforce/fivetran/dimensional/__install_dimensional.sql
+++ salesforce/fivetran/dimensional/__install_dimensional.sql
@@ -5,13 +5,16 @@ USE SCHEMA SALESFORCE_Z2S;
 CREATE OR REPLACE VIEW "DIM_ACCOUNT" COPY GRANTS AS
 SELECT
     --Key
-    ACC.ID          AS "Account Id"
+    ACC.ID                  AS "Account Id"
     --Attributes
-    ,ACC.NAME       AS "Account"
-    ,ACC.TYPE       AS "Type"
-    ,ACC.PHONE      AS "Phone"
-    ,ACC.WEBSITE    AS "Website"
-    ,ACC.INDUSTRY   AS "Industry"
+    ,ACC.NAME               AS "Account"
+    ,ACC.TYPE               AS "Type"
+    ,ACC.PHONE              AS "Phone"
+    ,ACC.WEBSITE            AS "Website"
+    ,ACC.INDUSTRY           AS "Industry"
+    ,ACC.BILLING_CITY       AS "City"
+    ,ACC.BILLING_STATE      AS "State"
+    ,ACC.BILLING_COUNTRY    AS "Country"
 FROM
     ACCOUNT ACC
 WHERE
